5d36afd7311b8f65f761ba783833affb0a3ee2f1
xybruceliu <xingyuliu@Xingyus-MBP.wv.cc.cmu.edu>
2018-02-09 22:13:30 -0500

    worked version for scale

--- scale.py
+++ scale.py
@@ -8,37 +8,39 @@ def make2dList(rows, cols):
     for row in range(rows): a += [[0]*cols]
     return a
 
-def up(L, length):
-    
+def interpolate(L, length):
+    xp = np.linspace(0, len(L), num=len(L))
+    x = np.linspace(0, len(L), num=length)
+    fp = L
+    return np.interp(x, xp, fp)
 
-
-def down(L, length):
-
-
-
-
-
-def scale(file_acc, file, thres, scale_co):
+def scale(file_acc, file, thres, length):
 
     (start, end) = start_end.find_startend(file_acc, thres)
 
     data = np.genfromtxt(file, delimiter=",", names=True, dtype=None)
 
-    feat_LL = make2dList(len(data[0]), len(data))
+    feat_ll = make2dList(len(data[0]), len(data))  
+
+    feat_LL = np.array(feat_ll, dtype='float16')
 
     for i in range(len(feat_LL)):
         for j in range(len(feat_LL[0])):
             feat_LL[i][j] = data[j][i]
-    timestamp = feat_LL.pop(0)
 
-    result = make2dList(len(data[0])-1, start-end+1)
+    feat_LL = feat_LL[1::]
 
-    for i in range(len(result)):
-        result[i] = feat_LL[i][start:end]
+    result0 = make2dList(len(data[0])-1, start-end+1)
 
-    return result
+    for i in range(len(result0)):
+        result0[i] = interpolate(feat_LL[i][start:end], length)
 
+    result = np.array(result0, dtype='float16')
 
+    print(len(result[0]))
+    print(result)
+
+    return np.reshape(result, (3, 10, 10))
 
 
 
