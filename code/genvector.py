3fc70ae027ce2c70043fb003c381ff585925eb9d
xybruceliu <xingyuliu@Xingyus-MBP.wv.cc.cmu.edu>
2018-02-10 02:02:15 -0500

    Worked demo without interface!

--- code/genvector.py
+++ code/genvector.py
@@ -9,12 +9,8 @@ def gen_samp_feat(samplepath):
 	vector = []
 	for axis in raw_data:
 		for time_frame in axis:
-			dft_frame = fft(time_frame)
-			energy = find_trans_energy(dft_frame)
-			entropy = find_trans_entropy(dft_frame)
-			mean = find_trans_mean(dft_frame)
-			deviation = find_ori_deviation(time_frame)
-			vector.extend([energy, entropy, mean, deviation])
+			for x in time_frame:
+				vector.append(x)
 	return vector	
 
 
