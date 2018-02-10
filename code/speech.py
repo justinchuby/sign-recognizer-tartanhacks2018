46a5596bcc791c079aabd41a9fcafccf06e7cb79
Jacky Zou <zoujiaqijacky@foxmail.com>
2018-02-10 13:13:14 -0500

    finished Speech Recognition feature

--- code/speech.py
+++ code/speech.py
@@ -12,7 +12,7 @@ try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("He/She said: " + r.recognize_google(audio))
+    print("You said: " + r.recognize_google(audio))
 except sr.UnknownValueError:
     print("Could not understand audio")
 except sr.RequestError as e:
