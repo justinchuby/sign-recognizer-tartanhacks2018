9b911f15495c833d027abdf86091c997ee13cc13
Candy Dong <qingyid@andrew.cmu.edu>
2018-02-09 22:31:31 -0500

    snapshot
    snapshot

---
+++ genvector.py
@@ -0,0 +1,11 @@
+from __future__ import division, print_function
+from dft import *
+from scale import *
+
+
+def read_
+#read gyro and orientation files
+for filename in os.listdir(path):
+    if not (filename.startswith('.')):
+        result.append(read_one(path_true + "/" + filename))
+
